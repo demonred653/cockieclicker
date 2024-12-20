d11107419d6b3d79ce330601bd937979481b429c
demonred653 <redwolf201117@gmail.com>
2024-12-20 08:55:47 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,36 +14,36 @@ basic.forever(function () {
 })
 basic.forever(function () {
     basic.showNumber(game.score())
-    if (game.score() > 101) {
+    if (game.score() > 1001) {
     	
     }
-    if (game.score() == 10 || game.score() == 20) {
+    if (game.score() == 100 || game.score() == 200) {
         for (let index = 0; index < 1; index++) {
             music.play(music.tonePlayable(131, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         }
     }
-    if (game.score() == 30 || game.score() == 40) {
+    if (game.score() == 300 || game.score() == 400) {
         for (let index = 0; index < 1; index++) {
             music.play(music.tonePlayable(147, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         }
     }
-    if (game.score() == 50 || game.score() == 60) {
+    if (game.score() == 500 || game.score() == 600) {
         for (let index = 0; index < 1; index++) {
             music.play(music.tonePlayable(165, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         }
     }
-    if (game.score() == 70 || game.score() == 80) {
+    if (game.score() == 700 || game.score() == 800) {
         for (let index = 0; index < 1; index++) {
             music.play(music.tonePlayable(175, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         }
     }
-    if (game.score() == 90) {
+    if (game.score() == 900) {
         for (let index = 0; index < 1; index++) {
             music.play(music.tonePlayable(196, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         }
     }
     for (let index = 0; index < 1; index++) {
-        if (game.score() == 100) {
+        if (game.score() == 1000) {
             music.play(music.stringPlayable("C5 F E B C5 - - - ", 120), music.PlaybackMode.UntilDone)
             basic.showLeds(`
                 . . . . .
@@ -81,7 +81,7 @@ basic.forever(function () {
                 . . . . .
                 `)
             game.addScore(1)
-            for (let index = 0; index < 0; index++) {
+            for (let index = 0; index < 1; index++) {
                 basic.clearScreen()
                 basic.showString("RESET? CLICK B")
             }
